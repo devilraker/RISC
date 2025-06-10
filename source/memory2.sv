a27bc5bc3a51af7d7fd7890ce3d1a87e3284d393
devilraker <vaishnavkrishna101@gmail.com>
2025-06-10 18:09:35 +0530

    made_ram_using_logic_ig

--- source/memory2.sv
+++ source/memory2.sv
@@ -21,11 +21,12 @@ module memory(
         if (memory_read) begin
             read_data = memory_array[addr[11:2]]; // Read from memory
             case(func3) begin
-                3'b000: read_data = $signed(memory_array[addr[11:2]]);  //load byte)
-                3'b001: read_data = $signed(emory_array[addr[7:2]]); // load it into signed halfword
-                3'b010: read_data = memory_array[addr] // 32 bit load word
-                3'b100: read_data = $unsigned(memory_array[addr[11:2]]); // load byte unsigned
-                3'b101: read_data = $unsigned(memory_array[addr[7:2]]); // load halfword unsigned
+                3'b000: read_data = {{24{memory_array[addr[11:2]][8*addr[1:0]+7]}}, memory_array[addr[11:2]][8*addr[1:0]+: 8]};  //load byte)
+                3'b001: read_data = {{16{memory_array[addr[11:2]][16*addr[1]+15]}}, memory_array[addr[11:2]][16*addr[1]+: 16]}; // load it into signed halfword
+                3'b010: read_data = memory_array[addr[11:2]]; // 32 bit load word
+                3'b100: read_data = {24'b0,memory_array[addr[11:2]][8*addr[1:0]+: 8]}; // load byte unsigned
+                3'b101: read_data = {16'b0,memory_array[addr[11:2]][16*addr[1]+: 16]}; // load halfword unsigned
+                default:read_data = 32'b0; // Default case, should not happen
             end
             endcase
         end
@@ -34,11 +35,22 @@ module memory(
     always_ff(posedge clk) begin
         if memory_write begin
             case(func3) begin
-                3'b000: memory_array[addr[11:2]] <= {memory_array[addr[11:2]][31:8], write_data[7:0]}; // Store byte
-                3'b001: memory_array[addr[11:2]] <= {memory_array[addr[11:2]][31:16], write_data[15:0], memory_array[addr[11:2]][7:0]}; // Store halfword
+                3'b000: begin
+                    case (addr[1:0])
+                        2'b00 : memory_array[addr[11:2]] <= {memory_array[addr[11:2]][31:8], write_data[7:0]};
+                        2'b01 : memory_array[addr[11:2]] <= {memory_array[addr[11:2]][31:16], write_data[7:0], memory_array[addr[11:2]][7:0]};
+                        2'b10 : memory_array[addr[11:2]] <= {memory_array[addr[11:2]][31:24], write_data[7:0], memory_array[addr[11:2]][15:0]};
+                        2'b11 : memory_array[addr[11:2]] <= {write_data[7:0], memory_array[addr[11:2]][23:0]}; 
+                    endcase
+                end
+                3'b001: begin
+                    case (addr[1])
+                        1'b0 : memory_array[addr[11:2]] <= {memory_array[addr[11:2]][31:16], write_data[15:0]};
+                        1'b1 : memory_array[addr[11:2]] <= {write_data[15:0],memory_array[addr[11:2]][31:16]}; // Store halfword
+                    endcase
+                end
                 3'b010: memory_array[addr[11:2]] <= write_data; // Store word
-                3'b100: memory_array[addr[11:2]] <= {memory_array[addr[11:2]][31:8], write_data[7:0]}; // Store byte unsigned
-                3'b101: memory_array[addr[11:2]] <= {memory_array[addr[11:2]][31:16], write_data[15:0], memory_array[addr[11:2]][7:0]}; // Store halfword unsigned
+                default : begin end
             end
             endcase
         end
